97dac7aeda25e42b5f3f7d7d63caeadcb7465e91
samir-javascript <samirjavascript00@gmail.com>
2024-02-28 00:05:30 +0100

    change

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,6 +1,6 @@
 import { Alert } from "react-bootstrap"
 
-const Message = ({variant, children }) => {
+const Message = ({variant, children }:any) => {
   return (
     <Alert className={`${variant === "danger" ? ' bg-red-100 text-[#ff5921]' : ''}  flex items-center mx-4 mt-3 mb-12 justify-center text-center`} variant={variant}>
         {children}
