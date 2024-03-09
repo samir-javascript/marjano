ee2e203ee7e5db86824413e184191454f0b92c5e
samir-javascript <samirjavascript00@gmail.com>
2024-03-09 19:12:03 +0100

    push and fix

--- app/(root)/admin/ordersList/page.tsx
+++ app/(root)/admin/ordersList/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/no-unescaped-entities */
+
 
 import {  Col, Row, Table } from "react-bootstrap";
 import Link from 'next/link'
