311cb3457d9e3d463887027c00e2b41f14416a00
samir-javascript <samirjavascript00@gmail.com>
2024-03-10 01:01:32 +0100

    push

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -6,7 +6,7 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
-  timeout: 60000,
+  timeout:820000,
 });
 
 export const POST = async (req: Request) => {
